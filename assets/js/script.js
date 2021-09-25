d8a23dfe5138edc7bc27136923e65f57c10b1d72
josephdaw <josephdaw@icloud.com>
2021-09-25 14:53:00 +0930

    add selectAnswer and reset functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,12 @@ const questions = [
             {text: "What happens if this has a lot of information", correct: false},
             {text: "8", correct: true}
         ]
+    },
+    {question: "Is this the last question",
+        answers: [
+            {text: "No", correct: false},
+            {text: "Yes", correct: true}
+        ]
     }
 ];
 
@@ -45,7 +51,7 @@ function playGame() {
     timerSectionEl.classList.remove("hide");
     // start the time counting down
     startTimer();
-    setNextQuestion();
+    showQuestion();
 
 }
 
@@ -62,24 +68,58 @@ function startTimer() {
 };
 
 // select the next question for the user to answer
-function setNextQuestion() {
-    showQuestion(questions[currentQuestionIndex]);
-};
+// function setNextQuestion() {
+//     showQuestion(questions[currentQuestionIndex]);
+// };
 
 // send the question to the document
-function showQuestion(question){
+function showQuestion(){
+    // set question to currentIndex of questions array
+    const question = questions[currentQuestionIndex];
+    // display question in header of question card
     questionEl.innerHTML = question.question;
+    // create a button for each answer associated with the question
     question.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerText = answer.text
         if (answer.correct){
+            // set the dataset to "correct" for the correct answer
             button.dataset.correct = answer.correct
         };
+
         button.addEventListener('click', selectAnswer)
         answerEl.appendChild(button);
     });
 };
 
-function selectAnswer() {
+function selectAnswer(el) {
+    const selectedAnswer = el.target;
+    const correct = selectedAnswer.dataset.correct;
+    if (correct) {
+        console.log("correct answer");
 
+        if (currentQuestionIndex < (questions.length - 1)) {
+            currentQuestionIndex++;
+            reset();
+            showQuestion();
+        } else {
+            console.log("no more questions");
+        };
+
+
+
+    } else {
+        console.log("incorrect");
+    }
+
+};
+
+function reset(){
+    // if there are answers we want to remove them
+    while (answerEl.firstChild){
+        // remove the answers
+        answerEl.removeChild
+        // check if there are still answers
+       (answerEl.firstChild)
+    }
 };
