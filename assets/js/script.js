4db4bc09f8db5fa2547c5da055df5cb4003a9a21
josephdaw <josephdaw@icloud.com>
2021-09-25 12:57:37 +0930

    add function to display question from array of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // define variables for the elements we will need to target
 const startButton = document.querySelector("#start-btn");
-const questionEl = document.querySelector("#question-card");
 
 const startSectionEl = document.querySelector(".start");
 const questionSectionEl = document.querySelector(".question");
@@ -8,24 +7,46 @@ const timerSectionEl = document.querySelector(".timer");
 
 const timerDisplayEl = document.querySelector(".timer-display");
 
+const questionEl = document.querySelector("#question");
+const answerEl = document.querySelector("#answers");
 
-
-var timer;
+var timer, currentQuestionIndex;
 var timerCount = 60;
+
+// questions and answers
+const questions = [
+    {question: "What is 2+2",
+        answers: [
+            {text: "4", correct: true},
+            {text: "22", correct: false}
+        ]
+    },
+    {question: "What is 4+4",
+        answers: [
+            {text: "2", correct: false},
+            {text: "8", correct: true}
+        ]
+    }
+];
 
 // addEvent to user clicking on "start-btn"
 startButton.addEventListener("click", playGame);
 
+
 // function that controls the game
 function playGame() {
+    // set current question to first in the array
+    currentQuestionIndex = 0;
     // once game has started, hide the 'start' button
     startSectionEl.classList.add('hide')
     // display the question section
     questionSectionEl.classList.remove("hide");
     // display the timer section
     timerSectionEl.classList.remove("hide");
-
+    // start the time counting down
     startTimer();
+    setNextQuestion();
+
 }
 
 function startTimer() {
@@ -38,4 +59,14 @@ function startTimer() {
             clearInterval(timer);
         }
     }, 1000);
-}
+};
+
+// select the next question for the user to answer
+function setNextQuestion() {
+    showQuestion(questions[currentQuestionIndex]);
+};
+
+// send the question to the document
+function showQuestion(question){
+    questionEl.innerHTML = question.question;
+};
